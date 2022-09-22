a84bab8cfcbc49d88d39a12724bf350a02588656
Mark <protectorate23@gmail.com>
2022-09-22 20:45:11 +0800

    int

--- phasebook/search.py
+++ phasebook/search.py
@@ -1,6 +1,10 @@
+from contextlib import nullcontext
+from types import NoneType
 from flask import Blueprint, request
 
 from .data.search_data import USERS
+
+from flask import request
 
 
 bp = Blueprint("search", __name__, url_prefix="/search")
@@ -9,6 +13,8 @@ bp = Blueprint("search", __name__, url_prefix="/search")
 @bp.route("")
 def search():
     return search_users(request.args.to_dict()), 200
+
+
 
 
 def search_users(args):
@@ -27,4 +33,51 @@ def search_users(args):
 
     # Implement search here!
 
-    return USERS
+    if len(args) == 0:
+        return USERS
+
+    id = request.args.get('id', default='0', type=str)
+
+    name = request.args.get('name', default='0', type=str)
+
+    age = request.args.get('age', default='0', type=str)
+
+    occupation = request.args.get('occupation', default='0', type=str)
+
+
+
+    id2 = [i for i in USERS if i['id'] == id]
+
+    name2 = [n for n in USERS if name in n['name']]
+
+    age2 = [a for a in USERS if a['age'] == int(age)or a['age'] == int(age) - 1 or a['age'] == int(age) + 1]
+
+    occupation2 = [o for o in USERS if occupation in o['occupation']]
+
+
+    
+    final = id2 + name2 + age2 + occupation2
+    
+    final2 = []
+
+
+    for f in range(len(final)):
+        if final[f] not in final[f + 1:]:
+            final2.append(final[f])
+
+    return sorted(final2, key=lambda d: d['id'])  
+
+
+    # Bonus Challenge
+
+    # return final2 
+    
+    
+
+
+    
+
+
+
+
+    
